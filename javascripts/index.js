5d4d0efc48783742be5d39b89a2e4334c895812d
Lianne Dakanno <liannedakanno@gmail.com>
2022-08-31 03:04:07 -0500

    changed variable names in searchForTea()

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,6 @@
 // DOMContentLoaded is used here because defer isnt in our script tags nor is our script
-// tag at the bottom of our body in the HTML file, and we want the content desplayed 
-document.addEventListener("DOMContentLoaded", () => {
+// tag at the bottom of our body in the HTML file, and we want the content displayed upon load
+document.addEventListener("DOMContentLoaded", e => {
     
     searchForTea();
 
@@ -11,32 +11,34 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-const ul = document.getElementById("searchedTea");
-
 
 //     searchForTea() will handle the form that is used to search for a tea name. The user
 //  will receive information related to the tea that they searched for only if the tea shop offers it on their menu. 
 //  If the tea shop does not have it, an alert will pop up suggesting a proper search term to use or a type of tea that is offered.
 function searchForTea() {
     
+    const div = document.getElementById("searchedTea");
     const form = document.getElementById("searchForm");
     form.addEventListener("submit", event => {
         
         event.preventDefault(); 
-        ul.textContent = "";
+        div.textContent = "";
         const userSearchInput = event.target.teaName.value.toLowerCase();
         
         
         fetch("http://localhost:3000/teas")
         .then(response => response.json())
         .then(data => {
-            
+            //try to make it address tea that aren't on menu
+            const notIncluded = data.includes(userSearchInput);
             data.find(element => {
                 const oneWordSearch = element.name.split(" ");
-                if ( userSearchInput === element.name.toLowerCase() || userSearchInput === oneWordSearch[0].toLowerCase()) {
-                    displaySearch(element);
-                } 
+                if ( userSearchInput === element.name.toLowerCase() || userSearchInput === element.name.split(" ")[0].toLowerCase()) {
+                    displaySearch(element, div);
+                };
             });
+
+            
             
         }).catch((error) => {
             console.log(error);
@@ -52,7 +54,7 @@ function searchForTea() {
 
 
 // displaySearch() will create the html elements that the data that is fetched will be placed in to be displayed
-function displaySearch(element) {
+function displaySearch(element, div) {
     
     const img = document.createElement("img");
     img.src = element.image
@@ -60,10 +62,10 @@ function displaySearch(element) {
     const h4 = document.createElement("h4");
     h4.textContent = element.name;
     
-    const li = document.createElement("li");
-    li.textContent = element.description + ` Flavor profile: ${element.flavorProfile}.`;
+    const p = document.createElement("p");
+    p.textContent = element.description + ` Flavor profile: ${element.flavorProfile}.`;
     
-    ul.append(img, h4, li);
+    div.append(img, h4, p);
     
 }
 
